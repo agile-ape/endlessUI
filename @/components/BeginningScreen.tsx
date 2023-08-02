79456e1f47c660608567712dd3a920a3fcb5db6f
firzhaardhia <ibrahimovic6690@gmail.com>
2023-08-02 17:03:51 +0700

    time left & next claim layout

--- @/components/BeginningScreen.tsx
+++ @/components/BeginningScreen.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import CustomConnectButton from './ui/connect-button';
 
 export default function BeginningScreen() {
@@ -9,8 +10,30 @@ export default function BeginningScreen() {
         <h1 className="uppercase mt-4 text-xl">are you the last man standing?</h1>
       </div>
 
-      <div className="mx-auto flex justify-center mt-20">
-        <CustomConnectButton />
+      <div className="mx-auto flex flex-col gap-5 mt-20">
+        <div className="flex justify-center">
+          <CustomConnectButton />
+        </div>
+        
+        <div className="text-center">
+          <div>
+            <p className="text-[20px]">Time Left: ?</p>
+          </div>
+          <h2 className="text-[40px]">04:10</h2>
+        </div>
+        <div className="bg-[#F6F6F6] border border-[#EBEBEB] text-center w-[220px] mx-auto rounded-lg">
+          <p className="font-extralight">Next Claim</p>
+          <div className="flex justify-center gap-3">
+            <p className="text-[36px]">0.500</p>
+            <Image
+              priority
+              src="/logo/cryptocurrency-color_eth.svg"
+              height={24}
+              width={24}
+              alt="ethereum logo"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
