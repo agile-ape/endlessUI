c8b26a7045a1dfcdcb2d0ed6afe0b788646a33cc
jokesquery <ibrahimovic6690@gmail.com>
2023-08-28 20:33:53 +0700

    countdown on timeleft

--- @/components/ui/Countdown.tsx
+++ @/components/ui/Countdown.tsx
@@ -1,13 +1,30 @@
 import { QuestionMarkCircledIcon } from '@radix-ui/react-icons';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { Button } from './button';
+import { useEffect, useState } from 'react';
+import { calculateTimeLeft } from '@/lib/utils';
+
+type TimeLeftType = {
+  // days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
 
 export default function Countdown() {
+  const [timeLeft, setTimeLeft] = useState<TimeLeftType>(calculateTimeLeft());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
   return (
     <div
       className="text-center bg-[#F6F6F6] dark:bg-[#1C1C1C]
     border border-[#EBEBEB] dark:border-[#444242]
-    text-center w-[220px] mx-auto rounded-lg p-2"
+    w-[220px] mx-auto rounded-lg p-2"
     >
       <div className="flex justify-center items-center gap-2">
         <p className="text-[20px]">Time Left</p>
@@ -26,10 +43,17 @@ export default function Countdown() {
           </Tooltip>
         </TooltipProvider>
       </div>
+      {/* use this button if the states supports change phase */}
       {/* <Button
         className="bg-[#31197B] text-white rounded-xl my-3"
       >Change Phase</Button> */}
-      <h2 className="text-[40px]">04:10</h2>
+
+      <h2 className="text-[40px]">
+        {/* {timeLeft.days ? `${timeLeft.days}:`: ""} */}
+        {timeLeft.hours ? `${timeLeft.hours}:`: ""}
+        {timeLeft.minutes}:{timeLeft.seconds}
+      </h2>
+
     </div>
   );
 }
--- @/lib/utils.ts
+++ @/lib/utils.ts
@@ -4,3 +4,25 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const calculateTimeLeft = () => {
+  const difference = +new Date("2023-09-01T19:00:00+05:30") - +new Date();
+  let timeLeft = {
+    // days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  };
+  if (difference > 0) {
+    timeLeft = {
+      // use this if u want to add days in timeleft
+      // days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      // hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      hours: Math.floor(difference / (1000 * 60 * 60)),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+
+  return timeLeft;
+};
