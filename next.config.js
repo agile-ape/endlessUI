371d6aa0b8dd476b7d588450148365caf2f066ae
fachry adhitya <ibrahimovic6690@gmail.com>
2023-09-10 14:15:08 +0700

    release whitelist page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     return config
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/whitelist',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
