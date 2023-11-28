7acc4418e0329f1596f3be2f233cb626dc420786
agile-ape <agileape@pm.me>
2023-11-28 17:44:21 +0800

    work a bit on admin

--- @/components/ui/Admin.tsx
+++ @/components/ui/Admin.tsx
@@ -11,8 +11,30 @@ import {
   DropdownMenuSubContent,
 } from '@/components/ui/dropdown-menu'
 import { Button } from './button'
+import { useAccount, useContractEvent, useContractReads } from 'wagmi'
+import { defaultContractObj } from '../../../services/constant'
 
 export default function Admin() {
+  const { isConnected } = useAccount()
+
+  const { data } = useContractReads({
+    contracts: [
+      {
+        ...defaultContractObj,
+        functionName: 'round',
+      },
+      {
+        ...defaultContractObj,
+        functionName: 'phase',
+      },
+      {
+        ...defaultContractObj,
+        functionName: 'nextTicketPrice',
+      },
+    ],
+    enabled: isConnected,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -25,8 +47,6 @@ export default function Admin() {
         align="end"
         className="bg-opacity-100 dark:bg-opacity-100 mt-2 container-last flex flex-col justify-center w-[30rem] p-5 bg-white"
       >
-        <div className="text-center">Gamemaster functions</div>
-
         <div className="w-[100%] text-zinc-800 dark:text-zinc-200">
           <div className="grid grid-cols-2 text-lg justify-between gap-1 text-xl">
             <p className="text-left">$LAST in wallet</p>
@@ -43,6 +63,8 @@ export default function Admin() {
             <p className="text-right"> 100 $LAST </p>
           </div>
         </div>
+
+        <div className="text-center">Gamemaster functions</div>
 
         <Button variant="primary" className="px-4 py-2 text-md rounded-xl">
           Start game
