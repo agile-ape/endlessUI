4a970ec9ae41272cd64b8c24041be4483aa1d093
agileape <agileape@pm.me>
2024-01-19 11:22:58 +0800

    fixed welcome modal

--- @/components/ui/WelcomeModal.tsx
+++ @/components/ui/WelcomeModal.tsx
@@ -20,7 +20,10 @@ const WelcomeModal: React.FC<WelcomeModalType> = ({ toggleModal }) => {
 
   return (
     <div className="fixed inset-0 z-50 bg-white/10 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-zinc-500/10">
-      <div className="flex justify-center fixed left-[50%] top-[50%] z-50 grid w-full md:w-full max-w-sm translate-x-[-50%] translate-y-[-50%] gap-4 p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-md sm:rounded-lg">
+      <div
+        ref={modalRef}
+        className="flex justify-center fixed left-[50%] top-[50%] z-50 grid w-full md:w-full max-w-sm translate-x-[-50%] translate-y-[-50%] gap-4 p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] rounded-md sm:rounded-lg"
+      >
         <div className="bg-white dark:bg-white p-0 shadow-lg w-[100%] rounded-3xl">
           <div
             className="rounded-3xl h-full w-full"
